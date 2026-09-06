07dc5c8b4e72cf2db413d1169fc315afe73c2a22
iJustWantToBeMe <88006446+iJustWantToBeMe@users.noreply.github.com>
2026-09-06 10:34:57 -0600

    Refactor: rename Login to Signin and FaceDetection to FaceRecognition. Rename component directories and files for App.js compatibility

--- src/App.js
+++ src/App.js
@@ -1,205 +1,180 @@
 import React, { Component } from 'react';
-import Particles from 'react-particles-js';
+import ParticlesBg from 'particles-bg'
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import FaceDetection from './components/FaceDetection/FaceDetection';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-const particlesOpts = {
-	particles: {
-		number: {
-			value: 130,
-			density: {
-				enable: true,
-				value_area: 900
-			}
-		}
-	},
-	interactivity: {
-		detect_on: 'window',
-		events: {
-			onhover: {
-				enable: true,
-				mode: 'repulse'
-			},
-			onclick: {
-				enable: true,
-				mode: 'bubble'
-			},
-			resize: true
-		},
-		modes: {
-			grab: {
-				distance: 800,
-				line_linked: {
-					opacity: 1
-				}
-			},
-			bubble: {
-				distance: 500,
-				size: 20,
-				duration: 1,
-				opacity: 0.8,
-				speed: 3,
-				color: '#FFFAFA'
-			},
-			repulse: {
-				distance: 75,
-				duration: 0.4
-			},
-			push: {
-				particles_nb: 4
-			},
-			remove: {
-				particles_nb: 2
-			}
-		}
-	},
-	retina_detect: true
-};
+const MODEL_ID = 'face-detection';
 
-const initialState = {
-	input: '',
-	imageUrl: '',
-	box: [],
-	route: 'login',
-	isSignedIn: false,
-	user: {
-		id: '',
-		name: '',
-		email: '',
-		entries: 0,
-		joined: ''
-	}
-};
+const returnClarifaiRequestOptions = (imageUrl) => {
+  const PAT = 'YOUR_PAT_HERE';
+  const USER_ID = 'clarifai';       
+  const APP_ID = 'main';
+
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+
+  return {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+  };
+}
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = initialState;
-	}
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      boxes: [],
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
+    }
+  }
 
-	loadUser = data => {
-		this.setState({
-			user: {
-				id: data.id,
-				name: data.name,
-				email: data.email,
-				entries: data.entries,
-				joined: data.joined
-			}
-		});
-	};
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
 
-	calcutlateFaceLocation = data => {
-		const image = document.getElementById('inputImage');
-		const width = Number(image.width);
-		const height = Number(image.height);
-		return data.outputs[0].data.regions.map(faces => {
-			const clarifaiFaceBox = faces.region_info.bounding_box;
-			return {
-				leftCol: clarifaiFaceBox.left_col * width,
-				topRow: clarifaiFaceBox.top_row * height,
-				rightCol: width - clarifaiFaceBox.right_col * width,
-				bottomRow: height - clarifaiFaceBox.bottom_row * height
-			};
-		});
-	};
+  createClarifaiBoundingBox = (pixelBox, imageWidth, imageHeight) => {
+    return {
+      left_col: pixelBox.xmin / imageWidth,
+      top_row: pixelBox.ymin / imageHeight,
+      right_col: pixelBox.xmax / imageWidth,
+      bottom_row: pixelBox.ymax / imageHeight
+    };
+  }
 
-	displayFaceBox = box => {
-		this.setState({ box: box });
-	};
+  calculateFaceLocation = (data) => {
+    const personItems = data.filter(item => item.label === "person");
+    const image = document.getElementById('inputimage');
+    const originalWidth = Number(image.naturalWidth);
+    const originalHeight = Number(image.naturalHeight);
+    const width = Number(image.width);
+    const height = Number(image.height);
 
-	onInputChange = evt => {
-		this.setState({ input: evt.target.value });
-	};
+    const boundingBoxes = personItems.map(person => {
+      const clarifaiFace = this.createClarifaiBoundingBox(person.box, originalWidth, originalHeight);
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+      };
+    });
 
-	onImgSubmit = () => {
-		this.setState({ imageUrl: this.state.input });
-		fetch('http://localhost:3000/imageurl', {
-			method: 'post',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({
-				input: this.state.input
-			})
-		})
-			.then(response => response.json())
-			.then(response => {
-				if (response) {
-					fetch('http://localhost:3000/image', {
-						method: 'put',
-						headers: { 'Content-Type': 'application/json' },
-						body: JSON.stringify({
-							id: this.state.user.id
-						})
-					})
-						.then(response => response.json())
-						.then(count => {
-							this.setState(Object.assign(this.state.user, { entries: count }));
-						})
-						.catch(console.log);
-				}
-				this.displayFaceBox(this.calcutlateFaceLocation(response));
-			})
-			.catch(err => console.log(err));
-	};
+    return boundingBoxes;
+  }
 
-	onRouteChange = route => {
-		this.setState({ route: route });
-		route === 'logout'
-			? this.setState(initialState)
-			: route === 'home'
-			? this.setState({ isSignedIn: true })
-			: this.setState({ isSignedIn: false });
-	};
+  displayFaceBox = (boxes) => {
+    this.setState({boxes: boxes});
+  }
 
-	// 			Alt. Code
-	// onRouteChange = (route) => {
-	//   if (route === 'logout') {
-	//     this.setState({ isSignedIn: false })
-	//   } else if (route === 'home') {
-	//     this.setState({ isSignedIn: true })
-	//   }
-	//   this.setState({ route: route });
-	// }
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
 
-	render() {
-		const { isSignedIn, imageUrl, box, route } = this.state;
-		return (
-			<div className='App'>
-				<Particles className='particles' params={particlesOpts} />
-				<Navigation
-					isSignedIn={isSignedIn}
-					onRouteChange={this.onRouteChange}
-				/>
-				{route === 'home' ? (
-					<div>
-						<Logo />
-						<Rank
-							name={this.state.user.name}
-							entries={this.state.user.entries}
-						/>
-						<ImageLinkForm
-							onInputChange={this.onInputChange}
-							onImgSubmit={this.onImgSubmit}
-						/>
-						<FaceDetection box={box} imageUrl={imageUrl} />
-					</div>
-				) : route === 'login' ? (
-					<Login loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-				) : (
-					<Register
-						loadUser={this.loadUser}
-						onRouteChange={this.onRouteChange}
-					/>
-				)}
-			</div>
-		);
-	}
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    const sendImageToHuggingFaceWithFetch = async (imageUrl) => {
+      const response = await fetch(imageUrl);
+      const imageBlob = await response.blob();
+      const contentType = response.headers.get("content-type");
+
+      const apiResponse = await fetch("https://router.huggingface.co/hf-inference/models/facebook/detr-resnet-50",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${"YOUR_API_KEY_HERE"}`,
+            "Content-Type": contentType,
+          },
+          body: imageBlob,
+        },
+      );
+
+      const result = await apiResponse.json();
+      console.log(result) // try to experiment and see what the image detects!
+      this.displayFaceBox(this.calculateFaceLocation(result))
+    };
+    
+    sendImageToHuggingFaceWithFetch(this.state.input)
+  }
+   
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  render() {
+    const { isSignedIn, imageUrl, route, boxes } = this.state;
+    return (
+      <div className="App">
+        <ParticlesBg type="fountain" bg={true} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'
+          ? <div>
+              <Logo />
+              <Rank
+                name={this.state.user.name}
+                entries={this.state.user.entries}
+              />
+              <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceDetection boxes={boxes} imageUrl={imageUrl} />
+            </div>
+          : (
+             route === 'signin'
+             ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
